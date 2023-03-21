7e53ae7f95eb69e3258be8a895ee223c1ef3a3c3
root <root@DESKTOP-O1KC17T>
2023-03-21 20:06:44 +0200

    add build commands

---
+++ render/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+pip install -r ./requirements/production.txt
+
+
+python manage.py migrate
+
+# exec /usr/local/bin/gunicorn config.wsgi --bind 0.0.0.0:80 --chdir=/app
